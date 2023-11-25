5b347c6eb82af0cebf407cc1b22d2856b06f437b
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-11-25 21:59:04 +0300

    section Projects

---
+++ src/app/components/ProjectTag.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  name: string;
+  isSelected: boolean;
+  onClick: (name: string) => void;
+}
+
+const ProjectTag = ({ name, onClick, isSelected }: Props) => {
+  const buttonStyles = isSelected
+    ? 'text-white border-purple-500'
+    : 'text-[#ADB7BE] border-slate-600 hover:border-white';
+
+  return (
+    <button
+      onClick={() => onClick(name)}
+      className={`border-2 rounded-full px-6 py-3 text-xl cursor-pointer ${buttonStyles}`}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default ProjectTag;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/app/components/HeroSection';
 import Navbar from '@/app/components/Navbar';
 import AboutSection from '@/app/components/AboutSection';
+import ProjectsSection from '@/app/components/ProjectsSection';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-10 py-4">
         <HeroSection/>
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   )
