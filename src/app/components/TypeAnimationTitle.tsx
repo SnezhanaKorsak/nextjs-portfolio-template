7831635c9b72668750b7928868a4047a49619bd7
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-11-25 20:16:58 +0300

    header section

---
+++ src/app/components/TypeAnimationTitle.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { TypeAnimation } from 'react-type-animation';
+
+const TypeAnimationTitle = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        'Snega',
+        2000,
+        'Web Developer',
+        2000,
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
+  );
+}
+
+export default TypeAnimationTitle;
