bf6d95a56788fb01d13508a6d0150d44a203587f
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-11-26 17:06:08 +0300

    fix NextApiRequest type

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,20 +1,13 @@
-import { NextResponse, NextApiRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 import EmailTemplate from '@/app/components/EmailTemplate';
 
-interface ExtendedNextApiRequest extends NextApiRequest {
-  body: {
-    email: string,
-    subject: string,
-    message: string,
-  },
-}
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: ExtendedNextApiRequest) {
-  const { email, subject, message } = req.body;
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { email, subject, message } = body;
   try {
     const data = await resend.emails.send({
       from: email,
