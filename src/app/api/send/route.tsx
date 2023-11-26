987edd95f203c0263b5ede86444dbb6df773745b
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-11-26 13:07:35 +0300

    fix

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,12 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { Resend } from 'resend';
 
 import EmailTemplate from '@/app/components/EmailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Response) {
-  const {email, subject, message} = await req.json();
+export async function POST(req: NextRequest) {
+  const {email, subject, message} = await req;
   try {
     const data = await resend.emails.send({
       from: email,
